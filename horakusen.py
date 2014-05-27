ad2fb1d31fcc56c4c1fc9db9c9926537d2f169a6
6379235442 <6379235442@mail.ecc.u-tokyo.ac.jp>
2014-05-27 16:05:28 +0900

    とりあえず左右対称まで。

--- horakusen.py
+++ horakusen.py
@@ -14,13 +14,12 @@ def subplots():
     for spine in ['right', 'top']:
         ax.spines[spine].set_color('none')
     
-    ax.grid()
     return fig, ax
 
 
 fig, ax = subplots()  # Call the local version, not plt.subplots()
-x = np.linspace(-2, 10, 200)
-y = f(x, t=1)
-ax.plot(x, y, 'r-', linewidth=2, label='sine function', alpha=0.6)
-ax.legend(loc='lower right')
+x = np.linspace(-5,5,2)
+for n in range(21):
+	y = f(x, t=-5+0.5*n)
+	ax.plot(x, y, 'r-', linewidth=2, alpha=0.6)
 plt.show()
