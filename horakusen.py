486dc3d2276b3a607ebb2132bd86b6e6eacd3276
6379235442 <6379235442@mail.ecc.u-tokyo.ac.jp>
2014-05-28 16:41:01 +0900

    最低限

--- horakusen.py
+++ horakusen.py
@@ -1,25 +1,36 @@
+from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import *
+from mpl_toolkits.axes_grid.axislines import SubplotZero
 
 def f(x, t):
     return t * x - t**2
+	
+fig = plt.figure(1)
+ax = SubplotZero(fig, 111)
+fig.add_subplot(ax)
 
-def subplots():
-    "Custom subplots with axes throught the origin"
-    fig, ax = plt.subplots()
+ax.axhline(linewidth=1.2, color="black")
+ax.axvline(linewidth=1.2, color="black")
 
-    # Set the axes through the origin
-    for spine in ['left', 'bottom']:
-        ax.spines[spine].set_position('zero')
-    for spine in ['right', 'top']:
-        ax.spines[spine].set_color('none')
-    
-    return fig, ax
+for direction in ["xzero", "yzero"]:
+        ax.axis[direction].set_axisline_style("-|>")
+        ax.axis[direction].set_visible(True)
 
+for direction in ["left", "right", "bottom", "top"]:
+	ax.axis[direction].set_visible(False)
+	
+plt.figtext(0.93, 0.37, 'a')  
+plt.figtext(0.505, 0.95, 'b')
 
-fig, ax = subplots()  # Call the local version, not plt.subplots()
+plt.xticks([])
+plt.yticks([])		
+ylim(-3.5,6.5)
+
 x = np.linspace(-5,5,2)
-for n in range(21):
-	y = f(x, t=-5+0.5*n)
-	ax.plot(x, y, 'r-', linewidth=2, alpha=0.6)
+a = np.linspace(-2,2,12)
+for t in a:
+	y = f(x, t)
+	ax.plot(x, y, 'k-', linewidth=1.0, alpha=0.6)
 plt.show()
