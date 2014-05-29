3384076a6bd40ec1b8138332b9a7f0e1d1a1fdda
6379235442 <6379235442@mail.ecc.u-tokyo.ac.jp>
2014-05-29 16:30:24 +0900

    細かいところで分からない部分がちょいちょいありますが、一応完成しました。

--- horakusen.py
+++ horakusen.py
@@ -1,36 +1,55 @@
-from __future__ import division
+# -*- coding: utf-8 -*-
+
 import matplotlib.pyplot as plt
 import numpy as np
-from pylab import *
 from mpl_toolkits.axes_grid.axislines import SubplotZero
 
-def f(x, t):
-    return t * x - t**2
-	
+
+# 図の背景の諸体裁を設定
+
+# 作図スペースを用意(?)。個々のコードの意味がわかりません。	
 fig = plt.figure(1)
 ax = SubplotZero(fig, 111)
 fig.add_subplot(ax)
 
+# 軸の設定
 ax.axhline(linewidth=1.2, color="black")
 ax.axvline(linewidth=1.2, color="black")
 
+# 軸に矢印
 for direction in ["xzero", "yzero"]:
-        ax.axis[direction].set_axisline_style("-|>")
-        ax.axis[direction].set_visible(True)
+	ax.axis[direction].set_axisline_style("-|>")
+	ax.axis[direction].set_visible(True)
 
+# 四方のaxis(?)、spine(?)を消す	
 for direction in ["left", "right", "bottom", "top"]:
 	ax.axis[direction].set_visible(False)
-	
-plt.figtext(0.93, 0.37, 'a')  
-plt.figtext(0.505, 0.95, 'b')
 
+# 軸に名前を付ける。位置は適宜設定。	
+plt.figtext(0.93, 0.37, 'x')  
+plt.figtext(0.505, 0.95, 'y')
+
+# 軸の目盛を消す。表示するy軸の範囲を設定(グラフが見易くなるよう適宜設定)。
 plt.xticks([])
 plt.yticks([])		
-ylim(-3.5,6.5)
+plt.ylim(-3.5,6.5)
 
-x = np.linspace(-5,5,2)
-a = np.linspace(-2,2,12)
-for t in a:
-	y = f(x, t)
+
+# 図を描く条件設定
+
+# 元になる関数を定義
+def f(x, t):
+    return t * x - t**2
+
+# 変数
+p = 5    #xの範囲　-p<=x<=p (左右対称にするため)
+r = 2    #tの範囲　-r<=t<=r (同上)
+n = 13   #接線の本数
+
+# 包絡線を作る
+x = np.linspace(-p,p,2)    # 直線なのでプロットする点は２点でいいかと。xはリストと同じ性質(?)なのに、どうしてf関数のxに入れるのかわかりません。(arrayだから？)
+t = np.linspace(-r,r,n-1)  # 傾きはn-1等分で均等に。lispaceで範囲内をn-1等分したarray(配列？)を用意。
+for i in t:
+	y = f(x, t=i)
 	ax.plot(x, y, 'k-', linewidth=1.0, alpha=0.6)
 plt.show()
